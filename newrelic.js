b27bb7f9afb0967a3814fffb0b6ff6c7c133e8f9
jjwdowik <jjwdowik@umich.edu>
2019-02-17 12:49:52 -0500

    modify newrelic

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,8 @@
+'use strict'
 /**
  * New Relic agent configuration.
  *
- * See lib/config.defaults.js in the agent distribution for a more complete
+ * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
 exports.config = {
@@ -12,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '3a543877c6d9fe23eb5cd64b8f1e5e8f38b99ca5',
+  license_key: process.env.NEW_RELIC_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
@@ -20,5 +21,33 @@ exports.config = {
      * production applications.
      */
     level: 'info'
+  },
+  /**
+   * When true, all request headers except for those listed in attributes.exclude
+   * will be captured for all traces, unless otherwise specified in a destination's
+   * attributes include/exclude lists.
+   */
+  allow_all_headers: true,
+  attributes: {
+    /**
+     * Prefix of attributes to exclude from all destinations. Allows * as wildcard
+     * at end.
+     *
+     * NOTE: If excluding headers, they must be in camelCase form to be filtered.
+     *
+     * @env NEW_RELIC_ATTRIBUTES_EXCLUDE
+     */
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
   }
 }
