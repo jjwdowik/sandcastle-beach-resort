b5e6209f0db0224c752eec4efac56e7632be6954
Jeremy Wdowik <jjwdowik@umich.edu>
2020-07-19 15:01:41 -0400

    remove contact me from form

--- routes/index.js
+++ routes/index.js
@@ -51,8 +51,6 @@ exports = module.exports = function(app) {
 
   app.get('/booking', routes.views.booking);
 
-	app.all('/contact', routes.views.contact);
-
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
